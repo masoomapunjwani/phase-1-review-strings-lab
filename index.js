d59ce6b309feaea3b51d942588c09819ae8a247d
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-08-17 19:17:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = "Masooma Hasnain";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting =  `Welcome, ${currentUser.slice(0,1)}!`;
+
+
+
+
+
